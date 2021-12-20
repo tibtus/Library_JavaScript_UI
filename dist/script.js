61192f2605f0dabf11ed1d1be83ac6134af10f45
TibTus <tibtus@gmail.com>
2021-12-20 21:13:46 +0200

    #library_method-for-working-with-elements_edit_mod_4

--- dist/script.js
+++ dist/script.js
@@ -223,6 +223,53 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selecto
   return this;
 };
 
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.closest = function (selector) {
+  let counter = 0;
+
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector);
+    counter++;
+  }
+
+  const objLength = Object.keys(this).length;
+
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function () {
+  let numberOfItems = 0;
+  let counter = 0;
+  const copyObj = Object.assign({}, this);
+
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+
+      this[counter] = arr[j];
+      counter++;
+    }
+
+    numberOfItems += arr.length - 1;
+  }
+
+  this.length = numberOfItems;
+  const objLength = Object.keys(this).length;
+
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+
+  return this;
+};
+
 /***/ }),
 
 /***/ "./src/js/lib/modules/classes.js":
@@ -396,7 +443,11 @@ $('button').on('click', function () {
 $('div').click(function () {
   console.log($(this).index());
 });
-console.log($('div').eq(2).find('.some'));
+/* console.log($('div').eq(2).find('.some')); */
+
+/* console.log($('.some').closest('.findme').addClass('testclass')); */
+
+console.log($('.more').eq(0).siblings());
 /* console.log($('button').html("СЮДА НАЖМИТЕ")); */
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@ $('div').click(function () {
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.some'));
+/* console.log($('div').eq(2).find('.some')); */
+/* console.log($('.some').closest('.findme').addClass('testclass')); */
+console.log($('.more').eq(0).siblings());
 
 /* console.log($('button').html("СЮДА НАЖМИТЕ")); */
